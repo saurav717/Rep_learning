d3169afa5d7f7e00217a1f6b3fc853693eb13dc0
saurav <es16btech11007>
2018-09-16 12:43:30 +0530

    working, nneed to change input format

--- Q1_A1.py
+++ Q1_A1.py
@@ -19,13 +19,38 @@ def centroid(G1):
     return Cen
 
 def total(G,X,K):
+        dst = 0
+        Ind = []
+        for i in range(0,250):
+            D = []   # keep this temporary
+            for j in range(0,K):
+                D = np.append(D,dist(X[i],G[j]))
+            Ind = np.append(Ind,int(np.argmin(D,0)))
+            #D1.append(D1,D[D.index(min(D))])
 
+        #print(Ind)
+        #print(len(Ind))
+        Ind = np.reshape(Ind,(250,1))
+        X = np.hstack((X,Ind))
+        #print(X)
+        Kl = []
+        for i in range(0,K):
+            GS = []
+            for j in range(0,250):
+                    if(int(X[j][2]) == i):
+                        GS = np.append(GS,X[j])
+                        GS = np.reshape(GS,(len(GS)/3,3))
+            Kl = np.append(Kl,centroid(GS))
 
+        Kl = np.reshape(Kl,(K,2))
+        return Kl
 
 ########################################################
 ####### This is for generating points
 
 K = input('enter number of clusters')
+#x = input('number of examples')
+#y = input('dimensions')
 X = np.random.random(500)
 X = np.reshape(X,(250,2))
 
@@ -33,7 +58,7 @@ threshold = 0.015
 
 k = 250/K
 G = []
-
+error = 4
 #print(X[1])
 #print(X[1][1] - X[1][0])
 #print(X[2][1] - X[2][0])
@@ -44,35 +69,22 @@ for i in range(0,K):
 
 G = np.reshape(G,(K,2))
 #G = np.hstack((G,np.zeros((K,1))))
-print(G)
+#print(G)
 
-#dist(X[0], X[1])
-dst = 0
-Ind = []
-for i in range(0,250):
-    D = []   # keep this temporary
-    for j in range(0,K):
-        D = np.append(D,dist(X[i],G[j]))
-    Ind = np.append(Ind,int(np.argmin(D,0)))
-    #D1.append(D1,D[D.index(min(D))])
+Kl = G
 
-print(Ind)
-#print(len(Ind))
-Ind = np.reshape(Ind,(250,1))
-X = np.hstack((X,Ind))
-#print(X)
-Kl = []
-for i in range(0,K):
-    GS = []
-    for j in range(0,250):
-            if(int(X[j][2]) == i):
-                GS = np.append(GS,X[j])
-                GS = np.reshape(GS,(len(GS)/3,3))
-    Kl = np.append(Kl,centroid(GS))
+while(error > 0.015):
+    dst = 0
+    Kl = total(Kl, X, K)
+    for i in range (0,K):
+        dst += dist(Kl[i],G[i])
+    G = Kl
+    error = dst
+    print('error = ', error)
 
-Kl = np.reshape(Kl,(K,2))
-for i in range (0,K):
-    dst += dist(Kl[i],G[i])
-G = Kl
-print(G)
-print(dst)
+plt.plot(X.T[0],X.T[1],'bo')
+plt.plot(G.T[0],G.T[1],'ro')
+plt.grid()
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
